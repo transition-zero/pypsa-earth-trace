1452666ae22c8de618816643d601621e3db28c7c
ollie-bell <56110893+ollie-bell@users.noreply.github.com>
2024-06-24 16:37:02 +0100

    minor readme update and batch run script update

--- scripts-custom/batch_job_model_runs.sh
+++ scripts-custom/batch_job_model_runs.sh
@@ -1,8 +1,15 @@
 # Define the ISO codes you want to loop over
-iso_codes=("FJ" "FO" "TO" "GD" "KY" "GL" "TC" "KI" "VG" "MQ" "CY" "CK" "MS" "GI" "NE" "TL" "NR" "BM" "FK" "VI" "VC" "PM" "VU" "LR")
+iso_codes=("HK" "MO")
+
+# Set some other command parameters
+machine_type="n1-standard-8"
+disk_size_gb=128
+cores=4
+bucket="feo-pypsa-staging"
+
 # Loop over all iso codes
 for iso in "${iso_codes[@]}"
 do
     # Run the python submit-job command for each country
-    python submit_job --command "snakemake --cores 4 feo-pypsa-staging/networks/$iso/elec_s_1_ec_lcopt_1H.nc --configfile /mnt/disks/gcs/feo-pypsa-staging/country_configs/config.$iso.yaml" --image "europe-west2-docker.pkg.dev/tz-feo-staging/feo-pypsa/pypsa-earth-image" --image-tag "latest" --gcs-bucket-path "feo-pypsa-staging" --config-file ./country_configs/config.$iso.yaml --project-id "tz-feo-staging" --region "europe-west2" --machine-type "n1-standard-8"
+    python submit_job --command "snakemake --cores $cores $bucket/networks/$iso/elec_s_1_ec_lcopt_1H.nc --configfile /mnt/disks/gcs/$bucket/country_configs/config.$iso.yaml" --image "europe-west2-docker.pkg.dev/tz-feo-staging/feo-pypsa/pypsa-earth-image" --image-tag "latest" --gcs-bucket-path $bucket --config-file ./country_configs/config.$iso.yaml --project-id "tz-feo-staging" --region "europe-west2" --machine-type $machine_type --disk-size-gb $disk_size_gb
 done
