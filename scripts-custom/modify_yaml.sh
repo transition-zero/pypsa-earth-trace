56e300ec312c053e31e685992b88280dd81564e1
Thomas Kouroughli <tomkourou@icloud.com>
2024-07-18 14:41:53 +0100

    chore(scripts-custom): 🚨 fix linting

--- scripts-custom/modify_yaml.sh
+++ scripts-custom/modify_yaml.sh
@@ -13,7 +13,7 @@ for config_file in $CONFIG_DIR/config.*.yaml; do
     # Check if the file exists
     if [[ -f "$config_file" ]]; then
         echo "Processing $config_file"
-        
+
         # Run the Python script with the specified arguments
         python3 update_yaml.py --file_path "$config_file" --year_start $YEAR_START --load_scale $LOAD_SCALE --enable_rules
     fi
