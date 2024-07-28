a2f8dfacd5bb3167ca0ec4e6b1c8944a3955d769
Thomas Kouroughli <tomkourou@icloud.com>
2024-07-28 22:58:03 +0100

    wip: ♻️ modify code to be able to test on a single country. * work on fixes to get the job succesfully being triggered. * config arguments are still not being parsed correctly.

--- ClimateTRACE/scripts/batch_add_electricity.py
+++ ClimateTRACE/scripts/batch_add_electricity.py
@@ -23,55 +23,69 @@ IMAGE = "europe-west2-docker.pkg.dev/tz-feo-staging/feo-pypsa/pypsa-earth-image"
 IMAGE_TAG = "latest"
 
 TARGET = "add_electricity"
-CONFIGFILE = "./ClimateTRACE/configs/config.{iso}.yaml"
+CONFIGFILE = "../configs/config.{iso}.yaml"
 CONFIG = (
-    "'snapshots={{start: {year}-01-01, end: {year}-12-31}}' "
-    "'load_options={{scale: {scale}}}' "
-    "'run={{name: {iso}/{year}}}' "
-    "'cutouts={{cutout: {iso}}}' "
+    "snapshots={{start: {year}-01-01, end: {year}-12-31}} "
+    "load_options={{scale: {scale}}} "
+    "run={{name: {iso}/{year}}}"
+    "cutouts={{cutout: {iso}}}"
 )
 NCORES = 1
 SNAKEMAKE = (
-    f"snakemake --cores {NCORES} "
-    f"{TARGET} "
-    f"--configfile /mnt/disks/gcs/{BUCKET}/country_configs/config.{{iso}}.yaml "
-    f"--config {CONFIG} "
+    f"snakemake {TARGET} --cores {NCORES}"
+    "--configfile ./ClimateTRACE/configs/config.{iso}.yaml"
+    f"""--config {CONFIG} """
     f"--dry-run"
 )
 
 COMMAND = (
     f"python submit_job "
-    f"--command {SNAKEMAKE} "
-    f"--image {IMAGE} --image-tag {IMAGE_TAG} "
+    f"--image {IMAGE} "
+    f"--image-tag {IMAGE_TAG} "
     f"--gcs-bucket-path {BUCKET} "
-    f"--configfile {CONFIGFILE} "
     f"--project-id {PROJECT_ID} "
     f"--region {REGION} "
     f"--machine-type {{machine_type}} "
     f"--disk-size-gb {{disk_size_gb}} "
+    f"""--command '{SNAKEMAKE}' """
+    f"--configfile {CONFIGFILE} "
 )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weather-year", type=int, required=True)
+    parser.add_argument("--weather-year", type=int, required=False, default=2019)
+    parser.add_argument("--iso-codes", nargs="+", required=False, default=["JP"])
     args = parser.parse_args()
 
     configs = os.listdir("../configs")
-    iso_codes = [re.search("[A-Z]{2}", config).group() for config in configs]
+    # configs = os.listdir("ClimateTRACE/configs")
+    iso_codes = (
+        args.iso_codes
+        if args.iso_codes
+        else [re.search("[A-Z]{2}", config).group() for config in configs]
+    )
 
     year = args.weather_year
+
     df_demand_scale_factors = pd.read_csv(
         f"../trace_data/demand_scale_factors_{year}.csv",
         keep_default_na=keep_default_na,
         na_values=na_values,
     )
-
+    # df_demand_scale_factors = pd.read_csv(
+    #     f"ClimateTRACE/trace_data/demand_scale_factors_{year}.csv",
+    #     keep_default_na=keep_default_na,
+    #     na_values=na_values,
+    # )
+    print(iso_codes)
     for iso in iso_codes:
-        print(iso)
+        print(f"this is the iso: {iso}")
         machine_type = "n1-standard-4"  # TODO: determine based on iso_code
         disk_size_gb = 32  # TODO: determine based on iso_code
-        scale = df_demand_scale_factors.loc[lambda x: x.iso2 == iso, "scale_factor"].item()
+        scale = df_demand_scale_factors.loc[
+            lambda x: x.iso2 == iso, "scale_factor"
+        ].item()
 
         command = COMMAND.format(
             iso=iso,
@@ -80,4 +94,5 @@ if __name__ == "__main__":
             machine_type=machine_type,
             disk_size_gb=disk_size_gb,
         )
+        # breakpoint()
         p = subprocess.Popen(shlex.split(command))
