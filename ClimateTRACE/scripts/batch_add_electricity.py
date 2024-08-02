085b1e8ffc8df263a26d65464cc3a9ac3ff7e63e
Thomas Kouroughli <tomkourou@icloud.com>
2024-08-02 13:06:10 +0100

    wip(ClimateTRACE/scripts): ✨ add timer to batch solve scrpt

--- ClimateTRACE/scripts/batch_add_electricity.py
+++ ClimateTRACE/scripts/batch_add_electricity.py
@@ -5,6 +5,7 @@ import re
 import shlex
 import subprocess
 
+import time
 import pandas as pd
 from pandas._libs.parsers import STR_NA_VALUES
 
@@ -28,7 +29,8 @@ CONFIG = (
     "'snapshots={{start: {year}-01-01, end: {year_}-01-01}}' "
     "'load_options={{scale: {scale}}}' "
     "'run={{name: {iso}/{year}}}' "
-    "'scenario={{ll: ['v1.25'], clusters: [10], opts: [1H]}}'"
+    "'scenario={{ll: ['v1.25'], clusters: [1], opts: [1H]}}' "
+    "'solving={{solver:{{threads: 1}}}}'"
 )
 SNAKEMAKE = (
     f"snakemake "
@@ -36,7 +38,6 @@ SNAKEMAKE = (
     f"{TARGET} "
     f"--configfile {CONFIGFILE} "
     f"--config {CONFIG} "
-    "--dry-run"
 )
 
 SYMLINK = (
@@ -81,7 +82,9 @@ if __name__ == "__main__":
         keep_default_na=keep_default_na,
         na_values=na_values,
     )
+    counter = 0
     print(iso_codes)
+
     for iso in iso_codes:
         print(f"this is the iso: {iso}")
         machine_type = "n1-standard-8"  # TODO: scale based on iso_code
@@ -117,3 +120,5 @@ if __name__ == "__main__":
             run_args = shlex.split(submit.replace(command, "")) + [command]
 
         p = subprocess.run(run_args)
+        print("Waiting for 5 minutes...")
+        time.sleep(5 * 60)  # Wait for 5 minutes
